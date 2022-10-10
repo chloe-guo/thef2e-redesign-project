943fcf3c680b1df003356eaf83ddbce0badaaf6e
Chloe <111554291+chloe-guo@users.noreply.github.com>
2022-10-10 19:31:15 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-vue-template
+這是一個 可以使用pug, sass，並含有gh-pages功能的vue專案。
+[Demo](https://chloe-guo.github.io/my-vue-template/)
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.home
+  img(src="@/assets/images/logo.png" alt="Vue logo")
+  HelloWorld(msg="Welcome to Your Vue.js App")
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<!-- <style lang="scss" scoped>
+</style> -->
