03d94cac843f0921cd12b54d0b50b92ea74236b3
Chloe <chloe.guo@hellosanta.com.tw>
2022-11-06 13:14:12 +0800

    feat:  animation on section 2

--- src/components/section_2_issue.vue
+++ src/components/section_2_issue.vue
@@ -1,23 +1,35 @@
 <template lang="pug">
 #issue
     ul.issue__lists
-        li.issue__item
+        li.issue__item.issue__item-1
+          .issue__content
             .issue__background
                 .issue-1-web
                     img(src="@/assets/images/s2-issue-1-web.gif")
                 .issue-1-car
                     img(src="@/assets/images/card-right.gif")
                 .issue-1-dialog.is-cubeFont
-                    .dialog__text I'm cool!
+                    .dialog__text
+                      span I
+                      sapn '
+                      span m
+                      span &nbsp;
+                      span c
+                      span o
+                      span o
+                      span l
+                      span !
             h6.issue__text.issue-1-text(data-stroke="羨慕別人的酷酷網頁動畫...") 羨慕別人的酷酷網頁動畫...
-        li.issue__item
+        li.issue__item.issue__item-2
+          .issue__content
             .issue__background
                 .issue-2-soldier
                     img(src="@/assets/images/s2-issue-2-soldier.png")
                 .issue-2-dialog.is-cubeFont
-                    .dialog__text !@#$%...
+                    .dialog__text
             h6.issue__text.issue-2-text(data-stroke="滿足不了同事的許願...") 滿足不了同事的許願...
-        li.issue__item
+        li.issue__item.issue__item-3
+          .issue__content
             .issue__background
                 .issue-3-tree
                     img(src="@/assets/images/s2-issue-3-tree.png")
@@ -28,9 +40,252 @@
 
 <script>
 import $ from "jquery";
+import { gsap, ScrollTrigger, EasePack } from "gsap/all";
+
 export default {
   name: "section_2_issue",
-  mounted: function () {},
+  mounted: function () {
+    gsap.registerPlugin(ScrollTrigger);
+    //===== issue__item-1 =====
+    const s2issue1TL = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".issue__item-1",
+        start: "-25% top",
+        end: "bottom top",
+        pin: true,
+        scrub: true,
+      },
+    });
+    gsap.set(".issue__item-1 .issue__content", {
+      opacity: 0,
+      xPercent: "-100",
+    });
+    s2issue1TL.to(".issue__item-1 .issue__content", {
+      opacity: 1,
+      xPercent: "0",
+      duration: 10,
+      ease: "power2.out",
+    });
+    gsap.set(".issue-1-web", {
+      opacity: 0,
+      scale: 0,
+    });
+    s2issue1TL
+      .to(
+        ".issue-1-web",
+        {
+          opacity: 1,
+          duration: 8,
+          ease: "power4.out",
+        },
+        "<"
+      )
+      .to(
+        ".issue-1-web",
+        {
+          opacity: 1,
+          scale: 1,
+          duration: 10,
+          ease: "power4.out",
+        },
+        "<"
+      );
+    gsap.set(".issue-1-car img", {
+      opacity: 0,
+      xPercent: "-50",
+    });
+    s2issue1TL.to(".issue-1-car img", {
+      opacity: 1,
+      xPercent: "0",
+      duration: 15,
+      ease: "back.out(0.7)",
+    });
+    gsap.set(".issue-1-dialog", {
+      opacity: 0,
+      scale: "0",
+    });
+    s2issue1TL.to(
+      ".issue-1-dialog",
+      {
+        opacity: 1,
+        scale: "1",
+        duration: 12,
+        delay: 5,
+        ease: "back.out(0.7)",
+      },
+      "<"
+    );
+    gsap.set(".issue-1-text", {
+      opacity: 0,
+      xPercent: "-50",
+    });
+    s2issue1TL
+      .to(
+        ".issue-1-text",
+        {
+          xPercent: "0",
+          duration: 18,
+          delay: 6,
+          ease: "power3.out",
+        },
+        "<"
+      )
+      .to(
+        ".issue-1-text",
+        {
+          opacity: 1,
+          duration: 25,
+          delay: 3,
+          ease: "power3.out",
+        },
+        "<"
+      );
+    s2issue1TL.to(".issue__item-1 .issue__content", {
+      opacity: 0,
+      yPercent: "-100",
+      duration: 20,
+      delay: 50,
+      ease: "power2.out",
+    });
+
+    //===== issue__item-2 =====
+    const s2issue2TL = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".issue__item-2",
+        start: "-25% top",
+        end: "bottom top",
+        pin: true,
+        scrub: true,
+      },
+    });
+    gsap.set(".issue__item-2 .issue__content", {
+      opacity: 0,
+      xPercent: "100",
+    });
+    s2issue2TL.to(".issue__item-2 .issue__content", {
+      opacity: 1,
+      xPercent: "0",
+      duration: 10,
+      ease: "power2.out",
+    });
+    gsap.set(".issue-2-soldier", {
+      opacity: 0,
+      xPercent: "50",
+    });
+    s2issue2TL.to(
+      ".issue-2-soldier",
+      {
+        opacity: 1,
+        xPercent: "0",
+        duration: 25,
+        ease: "power3.out",
+      },
+      "<"
+    );
+    gsap.set(".issue-2-dialog", {
+      opacity: 0,
+      scale: 0,
+    });
+    s2issue2TL.to(".issue-2-dialog", {
+      opacity: 1,
+      scale: 1,
+      duration: 12,
+      ease: "power1.out",
+    });
+    gsap.set(".issue-2-text", {
+      opacity: 0,
+      xPercent: "50",
+    });
+    s2issue2TL
+      .to(
+        ".issue-2-text",
+        {
+          xPercent: "-50",
+          duration: 18,
+          delay: 6,
+          ease: "power3.out",
+        },
+        "<"
+      )
+      .to(
+        ".issue-2-text",
+        {
+          opacity: 1,
+          duration: 25,
+          delay: 3,
+          ease: "power3.out",
+        },
+        "<"
+      );
+    s2issue2TL.to(".issue__item-2 .issue__content", {
+      opacity: 0,
+      yPercent: "-100",
+      duration: 20,
+      delay: 50,
+      ease: "power2.out",
+    });
+
+    //===== issue__item-3 =====
+    const s2issue3TL = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".issue__item-3",
+        start: "-25% top",
+        end: "bottom top",
+        pin: true,
+        scrub: true,
+      },
+    });
+    gsap.set(".issue__item-3 .issue__content", {
+      opacity: 0,
+      xPercent: "-100",
+    });
+    s2issue3TL.to(".issue__item-3 .issue__content", {
+      opacity: 1,
+      xPercent: "0",
+      duration: 10,
+      ease: "power2.out",
+    });
+    gsap.set(".issue-3-tree", {
+      opacity: 0,
+    });
+    s2issue3TL.to(
+      ".issue-3-tree",
+      {
+        opacity: 1,
+        duration: 20,
+        ease: "power3.out",
+      },
+      "<"
+    );
+    gsap.set(".issue-3-text", {
+      opacity: 0,
+      xPercent: "-50",
+    });
+    s2issue3TL
+      .to(".issue-3-text", {
+        xPercent: "0",
+        duration: 18,
+        delay: 6,
+        ease: "power3.out",
+      })
+      .to(
+        ".issue-3-text",
+        {
+          opacity: 1,
+          duration: 25,
+          delay: 3,
+          ease: "power3.out",
+        },
+        "<"
+      );
+    s2issue3TL.to(".issue__item-3 .issue__content", {
+      opacity: 0,
+      yPercent: "-100",
+      duration: 20,
+      delay: 50,
+      ease: "power2.out",
+    });
+  },
 };
 </script>
 
@@ -44,25 +299,29 @@ export default {
     clear: both;
   }
   &__item {
-    @include rect(980px, 570px);
-    background: $c-brand1;
-    & + .issue__item {
-      margin-top: calc($space-m * 10);
-    }
-    &:nth-child(2n + 1) {
-      float: left;
+    @include rect(100%, auto);
+    &-1,
+    &-3 {
+      @include flex(row, flex-start);
       .issue__text {
         @include poa(l, 0, 0, 0);
         transform: translateX(50%);
       }
     }
-    &:nth-child(2n) {
-      float: right;
+    &-2 {
+      @include flex(row, flex-end);
       .issue__text {
         @include poa(0, 0, r, 0);
         transform: translateX(-50%);
       }
     }
+    &-2,
+    &-3 {
+      margin-top: -30%;
+    }
+  }
+  &__content {
+    @include rect(980px, 570px);
   }
   &__text {
     display: block;
@@ -70,6 +329,7 @@ export default {
   }
   &__background {
     @include rect;
+    background: $c-brand1;
   }
   &-1 {
     &-web {
@@ -95,10 +355,35 @@ export default {
       @include bg;
       @include poa(58%, t, r, 86.5%);
       @include flex;
+      transform-origin: 10% bottom;
       .dialog__text {
         color: $c-brand1-dark;
         @include font(32px);
         transform: translateY(-38%);
+        span {
+          display: inline-block;
+          @for $i from 1 through 9 {
+            &:nth-child(#{$i}) {
+              animation: issue-1-dialog-text
+                0.6s
+                #{(9 - $i) *
+                -0.1}s
+                linear
+                infinite
+                alternate;
+            }
+          }
+          @keyframes issue-1-dialog-text {
+            0%,
+            100% {
+              transform: translateY(0);
+            }
+            45%,
+            55% {
+              transform: translateY(-6%);
+            }
+          }
+        }
       }
     }
   }
@@ -130,10 +415,30 @@ export default {
       @include bg;
       @include poa(l, t, 72%, 76%);
       @include flex;
+      transform-origin: 90% bottom;
       .dialog__text {
-        color: $c-brand1-dark;
-        @include font(32px);
+        @include flex;
         transform: translateY(-6%);
+        &:before {
+          content: "!@#$%...";
+          color: $c-brand1-dark;
+          @include font(32px);
+          animation: issue-2-dialog-text 1.2s linear infinite;
+          @keyframes issue-2-dialog-text {
+            0% {
+              content: "!@#$%...";
+            }
+            33% {
+              content: "*$%#!...";
+            }
+            66% {
+              content: "*&!@#...";
+            }
+            100% {
+              content: "*#$^&...";
+            }
+          }
+        }
       }
     }
   }
